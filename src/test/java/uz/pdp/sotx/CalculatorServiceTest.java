00371ab10bcb04bb919a7aaf984b914f1aca1df7
muhammad <olmospro1978@gmail.com>
2026-02-25 16:59:46 +0500

    shell

--- src/test/java/uz/pdp/sotx/CalculatorServiceTest.java
+++ src/test/java/uz/pdp/sotx/CalculatorServiceTest.java
@@ -1,15 +1,20 @@
 package uz.pdp.sotx;
 
+import jakarta.annotation.security.PermitAll;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 import java.util.Random;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-//@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CalculatorServiceTest {
 
     private static CalculatorService service;
@@ -21,31 +26,34 @@ class CalculatorServiceTest {
 
 
     @Test
-    @Order(2)
-    @DisplayName("kopi bilan 100 millisekundda javob qaytishi kerak")
-//    @TestMethodOrder
     void testDiv_shouldDoneOneMillisecond() {
         Assertions.assertTimeout(Duration.ofMillis(1), () -> {
             service.div(100, 100);
         });
     }
 
-    @Test
-    @DisplayName(value = "1 ga 2 ni qoshsa 3 chiqishi kerak enable bolishi kerak")
-    @EnabledIf(value = "isDisable")
+    @RepeatedTest(value = 100, name = "{displayName} | {currentRepetition} | {totalRepetitions}")
     void add() {
-        int a = 1;
-        int b = 2;
+        int a = new Random().nextInt(1, 100);
+        int b = new Random().nextInt(1, 100);
         int expected = a + b;
         int result = service.add(a, b);
 
         Assertions.assertEquals(expected, result);
     }
 
-    @Test
-    @DisplayName(value = "-1 ga -2 ni qoshsa -3 chiqishi kerak")
-    void testAddWithNegatives() {
-        int a = -1;
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    void testAddWithNegatives1(int a) {
+        int b = -2;
+        int expected = a + b;
+        int result = service.add(a, b);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = {"nums"})
+    void testAddWithNegatives2(int a) {
         int b = -2;
         int expected = a + b;
         int result = service.add(a, b);
@@ -53,46 +61,33 @@ class CalculatorServiceTest {
     }
 
 
-    @Test
-    @DisplayName(value = "2 ni 2 ga bolsa 1 chiqishi kerak")
-    @DisabledOnOs(value = OS.MAC)
-    void div() {
-        int a = 2;
-        int b = 2;
-        int expected = a / b;
-        int result = service.div(a, b);
+    @ParameterizedTest
+    @CsvSource(value = {
+            "a , b",
+            "1, 2",
+            "2, 3",
+            "12, 13",
+            "24, 31"
+    }, useHeadersInDisplayName = true)
+    void testAddWithNegatives3(int a, int b) {
+        int expected = a + b;
+        int result = service.add(a, b);
         Assertions.assertEquals(expected, result);
     }
 
-    // agar b=0   ->400 -> "not zero"
-    @Test
-//    @Disabled
-    @EnabledOnOs(value = {OS.LINUX, OS.MAC, OS.WINDOWS})
-    void testDiv_ShouldThrowBadRequestExceptionWhenBZero() {
-        int a = 2;
-        int b = 0;
-        assertThrows(BadRequestException.class, () -> {
-            service.div(a, b);
-        });
-
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("disable bolishi kerak")
-    @DisabledIf(value = "isDisable")
-    void testDiv_Should_Message_Not_ZeroMessageWhenBZero() {
-        int a = 2;
-        int b = 0;
-        RuntimeException exception = assertThrows(BadRequestException.class, () -> {
-            service.div(a, b);
-        });
-        Assertions.assertEquals("not zero", exception.getMessage());
+    @ParameterizedTest
+    @CsvFileSource(resources = "/test.csv", useHeadersInDisplayName = true)
+    void testAddWithNegatives4(int a, int b, int expected) {
+        int result = service.add(a, b);
+        Assertions.assertEquals(expected, result);
     }
 
 
-    boolean isDisable() {
-        return true;
+    public static Stream<Integer> nums() {
+        return Stream.of(
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        );
     }
+
 
 }
